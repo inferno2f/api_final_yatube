b5121eb99f6686cfbd68ec7c68ed4c8306e67779
Vlad Nikitin <nikitinv91@gmail.com>
2021-10-20 23:16:46 +0300

    pep8 fixes

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework import filters
-from django_filters.rest_framework import DjangoFilterBackend
 
 from .serializers import (
     CommentSerializer,
